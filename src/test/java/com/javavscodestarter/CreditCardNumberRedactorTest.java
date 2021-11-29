485d89bfaf2adb8f405969e8e0401653993af997
Matt Welke <mattwelke@gmail.com>
2021-11-29 13:30:23 -0500

    Refactor cc nums in parameterized test.

--- src/test/java/com/javavscodestarter/CreditCardNumberRedactorTest.java
+++ src/test/java/com/javavscodestarter/CreditCardNumberRedactorTest.java
@@ -9,48 +9,43 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class CreditCardNumberRedactorTest {
-  /**
-   * JUnit 5 arg provider.
-   * Fixture source: https://stripe.com/docs/testing
-   * @return
-   */
-  static Stream<Arguments> testRedactArgumentProvider() {
-    Stream<Arguments> creditCardNumbers = Stream.of(
+  // Fixture source: https://stripe.com/docs/testing
+  private static final Stream<String> creditCardNumbers = Stream.of(
       // Visa
-      Arguments.of("4242424242424242", new RedactResult(1, "PII_CREDIT_CARD")),
-      Arguments.of("4000056655665556", new RedactResult(1, "PII_CREDIT_CARD")),
+      "4242424242424242",
+      "4000056655665556",
 
       // Mastercard
-      Arguments.of("5555555555554444", new RedactResult(1, "PII_CREDIT_CARD")),
-      Arguments.of("2223003122003222", new RedactResult(1, "PII_CREDIT_CARD")),
-      Arguments.of("5200828282828210", new RedactResult(1, "PII_CREDIT_CARD")),
-      Arguments.of("5105105105105100", new RedactResult(1, "PII_CREDIT_CARD")),
+      "5555555555554444",
+      "2223003122003222",
+      "5200828282828210",
+      "5105105105105100",
 
       // American Express
-      Arguments.of("378282246310005", new RedactResult(1, "PII_CREDIT_CARD")),
-      Arguments.of("371449635398431", new RedactResult(1, "PII_CREDIT_CARD")),
+      "378282246310005",
+      "371449635398431",
 
       // Discover
-      Arguments.of("6011111111111117", new RedactResult(1, "PII_CREDIT_CARD")),
-      Arguments.of("6011000990139424", new RedactResult(1, "PII_CREDIT_CARD")),
+      "6011111111111117",
+      "6011000990139424",
 
       // Diners Club
-      Arguments.of("3056930009020004", new RedactResult(1, "PII_CREDIT_CARD")),
-      Arguments.of("36227206271667", new RedactResult(1, "PII_CREDIT_CARD")),
+      "3056930009020004",
+      "36227206271667",
 
       // JCB
-      Arguments.of("3566002020360505", new RedactResult(1, "PII_CREDIT_CARD")),
+      "3566002020360505",
 
       // UnionPay
-      Arguments.of("6200000000000005", new RedactResult(1, "PII_CREDIT_CARD"))
-    );
+      "6200000000000005");
 
-    return creditCardNumbers;    
+  static Stream<Arguments> testRedactArgumentProvider() {
+    return creditCardNumbers.map((String cc) -> Arguments.of(cc, new RedactResult(1, "PII_CREDIT_CARD")));
   }
 
   @ParameterizedTest
   @MethodSource("testRedactArgumentProvider")
-  void testRedact(String src, RedactResult expected) {
+  void testRedactSingleCcNoWhitespace(String src, RedactResult expected) {
     assertEquals(expected, new CreditCardNumberRedactor(src).redact());
   }
 }
